5e236f9d7d687e553589a81e9b06011962c8a685
osamazafar128@csu.fullerton.edu <Bigbang@5670006>
2019-03-27 15:32:10 -0700

    docker file updated

--- docker_bash.sh
+++ docker_bash.sh
@@ -18,7 +18,7 @@ docker logs sh
 
 #cd build/libs
 
-docker cp /home/travis/build/osamazafar/TravisTesting/build/libs sh:/home/TravisTesting.jar
+docker cp /build/libs/TravisTesting.jar sh:/opt/TravisTesting.jar
 
 java -jar TravisTesting.jar
 
