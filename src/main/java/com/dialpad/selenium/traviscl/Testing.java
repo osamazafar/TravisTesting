02d572388ce030b357b264a9acdf1e47e39652b8
osamazafar128@csu.fullerton.edu <Bigbang@5670006>
2019-04-18 18:52:33 -0700

    Testing code is running

--- src/main/java/com/dialpad/selenium/traviscl/Testing.java
+++ src/main/java/com/dialpad/selenium/traviscl/Testing.java
@@ -1,12 +1,17 @@
 package com.dialpad.selenium.traviscl;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -28,19 +33,20 @@ public class Testing
 	public void start() throws InterruptedException, MalformedURLException
 	{
 
-		//_options.setHeadless(true);
+		_options.setHeadless(true);
 	
-		//_options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors", "--remote-debugging-port=4444");
+		//_options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors");
 		_capabilities = DesiredCapabilities.chrome();
 		_capabilities.setCapability("version", "");
 		_capabilities.setPlatform(Platform.LINUX);
 		_capabilities.setJavascriptEnabled(true);
 		_capabilities.setCapability(ChromeOptions.CAPABILITY, _options);
-		_wd = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), _capabilities);
+		//_wd = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), _capabilities);
 
 		_wd = new ChromeDriver(_capabilities);
 		_wd.get("https://www.google.com/");
 		_logger.info(_wd.getTitle());
+		captureScreenshot("Screenshot #  0");
 	}
 	
 	public void action() throws InterruptedException
@@ -51,9 +57,11 @@ public class Testing
 		e.sendKeys(Keys.RETURN);
 		WebElement e1 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"rso\"]/div[1]/div/div[1]/div/div/div[1]/a/h3")));
 		e1.click();
+		captureScreenshot("Screenshot # 1");
 		_logger.info(_wd.getTitle());
 		WebElement e2 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[1]/div[5]/ul[1]/li[2]/a")));
 		e2.click();
+		captureScreenshot("Screenshot # 2");
 		_logger.info(_wd.getTitle());
 		
 		 ArrayList<String> tabs = new ArrayList<String>(_wd.getWindowHandles());
@@ -69,6 +77,19 @@ public class Testing
 		
 
 		_logger.info("its the end");
+	}
+	
+	public void captureScreenshot(String fileName)
+	{
+		File scrFile = ((TakesScreenshot) _wd).getScreenshotAs(OutputType.FILE);
+		try 
+		{
+			FileUtils.copyFile(scrFile, new File("/Users/osama/Downloads/screenshots" + fileName));
+		}
+		catch (IOException e)
+		{
+			e.getStackTrace();
+		}
 	}
 
 	
