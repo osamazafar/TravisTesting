ba38b9d2178dc9d170c109c87d5b3a99bbe893ba
osamazafar128@csu.fullerton.edu <Bigbang@5670006>
2019-04-26 09:38:49 -0700

    testing Travis

--- docker_bash.sh
+++ docker_bash.sh
@@ -12,11 +12,11 @@ docker run -d -p 4444:4444  --name sh  selenium/hub
 
 docker images
 
-
+sleep 5
 
 docker run -d  -p --link sh:hub -v /dev/shm:/dev/shm  --name chrome  selenium/node-chrome
 
-sleep 5
+
 
 docker ps -a
 
--- src/main/java/com/dialpad/selenium/traviscl/Testing.java
+++ src/main/java/com/dialpad/selenium/traviscl/Testing.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
+
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -14,7 +14,7 @@ import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -30,12 +30,13 @@ public class Testing
 	private DesiredCapabilities _capabilities;
 	private ChromeOptions _options = new ChromeOptions();
 	
+	
 	public void start() throws InterruptedException, MalformedURLException
 	{
 
 		
 		_options.addArguments("--headless");
-		//_options.addArguments("--disable-gpu");
+		_options.addArguments("--disable-gpu");
 		_options.addArguments("--window-size=1280,800");
 		_options.addArguments("--allow-insecure-localhost");
 		//_options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors");
@@ -44,35 +45,32 @@ public class Testing
 		_capabilities.setPlatform(Platform.LINUX);
 		_capabilities.setJavascriptEnabled(true);
 		_capabilities.setCapability(ChromeOptions.CAPABILITY, _options);
-		//_wd = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), _capabilities);
+		_wd = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), _capabilities);
 
-		_wd = new ChromeDriver(_options);
-		_wd.get("https://www.google.com/");
+		//_wd = new ChromeDriver(_options);
+		_wd.get("https://www.google.com/gmail/about");
 		_logger.info(_wd.getTitle());
-		captureScreenshot("Screenshot #  0");
+		 captureScreenshot("Screenshot #  0");
 	}
-	
+
 	public void action() throws InterruptedException
 	{
 		WebDriverWait wait = new WebDriverWait(_wd, 10);
 		WebElement e = wait.until(ExpectedConditions.elementToBeClickable(By.name("q")));
-		e.sendKeys("gmail.com");
+		e.sendKeys("gmail");
+		captureScreenshot("Screenshot #  0");
 		e.sendKeys(Keys.RETURN);
-		WebElement e1 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"rso\"]/div[1]/div/div[1]/div/div/div[1]/a/h3")));
+		Thread.sleep(5000);
+		WebElement e1 = wait.until(ExpectedConditions.elementToBeClickable(By.className("LC20lb")));
 		e1.click();
+		_logger.info(_wd.getTitle());
 		captureScreenshot("Screenshot # 1");
 		_logger.info(_wd.getTitle());
-		WebElement e2 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[1]/div[5]/ul[1]/li[2]/a")));
-		e2.click();
-		captureScreenshot("Screenshot # 2");
-		_logger.info(_wd.getTitle());
 		
-		 ArrayList<String> tabs = new ArrayList<String>(_wd.getWindowHandles());
-		 _wd.switchTo().window(tabs.get(1));
-		 Thread.sleep(200);
 		WebElement e3 = wait.until(ExpectedConditions.elementToBeClickable(By.name("identifier")));
 		e3.sendKeys("callback229@gmail.com");
 		e3.sendKeys(Keys.ENTER);
+		captureScreenshot("Screenshot # 2");
 		_logger.info(_wd.getTitle());
 		WebElement e4 = wait.until(ExpectedConditions.elementToBeClickable(By.name("password")));
 		e4.sendKeys("Dialpad100");
